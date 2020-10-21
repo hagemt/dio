957c1f4b77451ac0bde98b2332b82b0a6eb1b5eb
Tor Hagemann <tor.hagemann@socotra.com>
2020-10-21 08:40:23 -0700

    pages: better "hello" API and all votes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { URL } = require('url')
+
+const parseArray = (req, name, defaultValues = []) => {
+  const origin = `http://${req.headers.host}`
+  const parsed = new URL(req.url, origin)
+  const values = parsed.searchParams.getAll(name)
+  return (values.length > 0) ? values : defaultValues
+}
 
 export default (req, res) => {
+  const name = parseArray(req, 'name', ['John', 'Doe']).join(' ')
+  const body = {
+    name,
+  }
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json(body)
 }
